f7deb9dacb40b6baae67cbf717b196c20fd2787e
Alejandro Mendiola <alejandro.mendiola.colin@outlook.com>
2025-10-07 21:36:39 -0600

    fix supabase files

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,35 +1,29 @@
-import type {NextRequest} from "next/server";
-import type {NextResponse} from "next/server";
-import {createServerClient, type CookieOptions} from "@supabase/ssr";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { type NextRequest, type NextResponse } from "next/server";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+export function createSupabaseMiddlewareClient(
+  request: NextRequest,
+  response: NextResponse
+) {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables");
-}
-
-export function createSupabaseMiddlewareClient(request: NextRequest, response: NextResponse) {
+  if (!supabaseUrl || !supabaseAnonKey) {
+    throw new Error("MIDDLEWARE: Missing Supabase environment variables");
+  }
   return createServerClient(supabaseUrl, supabaseAnonKey, {
     cookies: {
       get(name: string) {
         return request.cookies.get(name)?.value;
       },
-      set(name: string, value: string, options?: CookieOptions) {
-        response.cookies.set({
-          name,
-          value,
-          ...options
-        });
+      set(name: string, value: string, options: CookieOptions) {
+        request.cookies.set({ name, value, ...options });
+        response.cookies.set({ name, value, ...options });
       },
-      remove(name: string, options?: CookieOptions) {
-        response.cookies.set({
-          name,
-          value: "",
-          ...options,
-          maxAge: 0
-        });
-      }
-    }
+      remove(name: string, options: CookieOptions) {
+        request.cookies.set({ name, value: "", ...options });
+        response.cookies.set({ name, value: "", ...options });
+      },
+    },
   });
 }
