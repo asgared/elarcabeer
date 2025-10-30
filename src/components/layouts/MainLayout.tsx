3bf619e22625bcf7cd168f860e54111f995376d7
Alejandro <32518332+asgared@users.noreply.github.com>
2025-10-29 18:18:54 -0600

    refactor: migrate legacy layouts from chakra

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,50 +1,52 @@
-import Head from "next/head"
-import { Box } from "@chakra-ui/react"
+import Head from "next/head";
+import {ReactNode} from "react";
+
 // import { Footer, Header } from "../ui"
+
 interface Props {
-	children: React.ReactNode | React.ReactNode[]
-	title: string
-	pageDescription: string
-	imageUrl?: string
+  children: ReactNode;
+  title: string;
+  pageDescription: string;
+  imageUrl?: string;
 }
 
 export default function MainLayout({
-	children,
-	imageUrl = "https://res.cloudinary.com/ljtdev/image/upload/v1678488373/WhatsApp_Image_2023-03-10_at_17.45.20_xdibu7.jpg",
-	pageDescription,
-	title,
+  children,
+  imageUrl = "https://res.cloudinary.com/ljtdev/image/upload/v1678488373/WhatsApp_Image_2023-03-10_at_17.45.20_xdibu7.jpg",
+  pageDescription,
+  title,
 }: Props) {
-	return (
-		<>
-			<Head>
-				<title>{title}</title>
-				<meta name='title' content={title} />
-				<meta name='description' content={pageDescription} />
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={pageDescription} />
 
-				<meta name='og:title' content={title} />
-				{imageUrl && <meta name='og:image' content={imageUrl} />}
-				<meta name='og:description' content={pageDescription} />
+        <meta name="og:title" content={title} />
+        {imageUrl && <meta name="og:image" content={imageUrl} />}
+        <meta name="og:description" content={pageDescription} />
 
-				<meta property='og:title' content={title} />
-				{imageUrl && <meta property='og:image' content={imageUrl} />}
-				<meta property='og:description' content={pageDescription} />
-				<meta property='og:type' content='website' />
-				<meta property='og:url' content='https://www.elarcabeer.com/' />
+        <meta property="og:title" content={title} />
+        {imageUrl && <meta property="og:image" content={imageUrl} />}
+        <meta property="og:description" content={pageDescription} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.elarcabeer.com/" />
 
-				<meta property='twitter:card' content='summary_large_image' />
-				<meta property='twitter:url' content='https://www.elarcabeer.com/' />
-				<meta property='twitter:title' content={title} />
-				<meta property='twitter:description' content={pageDescription} />
-				{imageUrl && <meta property='twitter:image' content={imageUrl} />}
-			</Head>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://www.elarcabeer.com/" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={pageDescription} />
+        {imageUrl && <meta property="twitter:image" content={imageUrl} />}
+      </Head>
 
-			{/* <Header /> */}
+      {/* <Header /> */}
 
-			<Box as={"main"} width={["100%", "100%", "90%", "80%"]} maxW={"1200px"} margin={"auto"} mb={18}>
-				{children}
-			</Box>
+      <main className="mx-auto mb-[4.5rem] w-full max-w-[1200px] md:w-[90%] lg:w-4/5">
+        {children}
+      </main>
 
-			{/* <Footer /> */}
-		</>
-	)
+      {/* <Footer /> */}
+    </>
+  );
 }
