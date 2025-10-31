ac1ac4b0886124dca7687c90a130da7e1b4779bd
Alejandro Mendiola <alejandro.mendiola.colin@outlook.com>
2025-10-31 11:57:15 -0600

    fix dependences

--- next.config.js
+++ next.config.js
@@ -2,8 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [{protocol: 'https', hostname: 'images.unsplash.com'}]
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // permite todos los paths dentro de Cloudinary
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
